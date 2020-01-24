78177675598c86779e13022e5487aa9ee6de94c5
Carlos <c.c.oviedo@hotmail.com>
2020-01-24 13:25:58 +0100

    Ahora si, de verdad de la buena

--- app-front-react/src/componentes/App.js
+++ app-front-react/src/componentes/App.js
@@ -1,33 +1,35 @@
-// Componente Raiz del proyecto
-
-import React from 'react';  // importamos mod React
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
-import logo from '../logo.svg'; // importamos el fich
+// Componente raiz del proyecto
+import React from "react";
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import logo from "../logo.svg";
 import './App.css';
-import CrearUsuario from'./CrearUsuario';
-import ListarUsuarios from './ListarUsuarios';
+import CrearUsuario from './CrearUsuario';
+import ListarUsuario from './ListarUsuarios';
 
-function App() {
-    let estiloLogo = {
-        width:"10em",
-        height:"10em"
+function App(){
+    let estiloLogo = { 
+        // Objeto de JS con propiedades CSS
+        width: "5em",
+        height: "5em"
+
     }
     return (
         <Router>
-        <div className="App">
-            <header className="App-header">
-                <img src={ logo } className="App-logo" alt="logo" />
-            <h1>Operaciones Crud Usuarios</h1>
-            </header>
-            <nav>
-            <Link to="/">Listado</Link>
-            <Link to='/registro'>Crear Usuarios</Link>
-            </nav>
-            <Route path='/' exact component={ListarUsuarios}></Route>
-            <Route path='/registro' component={CrearUsuario}></Route>
-        </div>
+            <div  className="App">
+                <header className="App-header">
+                    <img src={ logo } style={ estiloLogo }
+                        className="App-logo" alt="logo"/>
+                    <h1>Operaciones CRUD usuarios</h1>
+                </header>
+                <nav>
+                    <Link to="/">Listado</Link> - 
+                    <Link to="/registro"> Crear usuario</Link>
+                </nav>
+                <Route path="/" exact component={ ListarUsuario }/>
+                <Route path="/registro" component={ CrearUsuario }/>
+            </div>
         </Router>
-          
     );
 }
+
 export default App;
